90a23d0c6fd5221306769c9dec8888f6cfee6246
sraj8 <31904232+sraj8@users.noreply.github.com>
2017-12-08 04:17:22 -0600

    Spliiting of grammar and unit test added

--- JavaProgramGenerator/src/main/java/Utils/Generator.java
+++ JavaProgramGenerator/src/main/java/Utils/Generator.java
@@ -8,7 +8,17 @@ public class Generator {
 
     public static void generateTopLevelQueue(List<String> topList, Queue<String> topQueue){
         //populate top queue from top list
+        String splittedArray[]= {};
+        for (String grammar : topList) {
+            splittedArray = getSplittedArray(grammar);
+            for (String splitted : splittedArray) {
+                topQueue.add(splitted);
+            }
+        }
+    }
 
-
+    private static String[] getSplittedArray(String grammar) {
+        String[] splitted = grammar.split("\\s+");
+        return splitted;
     }
 }
